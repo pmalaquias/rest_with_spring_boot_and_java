00c3e1d3a47d458d964d4afdd9ad85131f18f45b
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-12-25 15:53:14 -0300

    Criando outras operações

--- 1.restWithSpringBootAndJava/src/main/java/com/malaquias/restwithspringbootandjava/MathController.java
+++ 1.restWithSpringBootAndJava/src/main/java/com/malaquias/restwithspringbootandjava/MathController.java
@@ -9,6 +9,8 @@ import java.util.concurrent.atomic.AtomicLong;
 public class MathController {
     private  final AtomicLong counter  = new AtomicLong();
 
+    /*Sum*/
+    // http://localhost:8080/sum/10/5
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
@@ -19,6 +21,78 @@ public class MathController {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
         return converteToDouble(numberOne) + converteToDouble(numberTwo);
+    }
+
+    /*Subtraction*/
+    // http://localhost:8080/subtraction/10/5
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtraction(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    )throws Exception{
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return converteToDouble(numberOne) - converteToDouble(numberTwo);
+    }
+
+    /*Multiplication*/
+    // http://localhost:8080/multiplication/10/5
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    )throws Exception{
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return converteToDouble(numberOne) * converteToDouble(numberTwo);
+    }
+
+    /*Division*/
+    // http://localhost:8080/division/10/5
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception{
+
+            if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+                throw new UnsupportedMathOperationException("Please set a numeric value!");
+            }
+            if(converteToDouble(numberTwo) == 0){
+                throw new UnsupportedMathOperationException("Division by zero is not allowed!");
+            }
+            return converteToDouble(numberOne) / converteToDouble(numberTwo);
+    }
+
+    /*Mean*/
+    // http://localhost:8080/mean/10/5
+    @RequestMapping(value = "/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double mean(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    )throws Exception{
+
+            if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+                throw new UnsupportedMathOperationException("Please set a numeric value!");
+            }
+            return (converteToDouble(numberOne) + converteToDouble(numberTwo)) / 2;
+    }
+
+    /*Square Root*/
+    // http://localhost:8080/squareRoot/9
+    @RequestMapping(value = "/squareRoot/{number}", method = RequestMethod.GET)
+    public Double squareRoot(
+            @PathVariable(value = "number") String number
+    )throws Exception{
+
+                if(!isNumeric(number)){
+                    throw new UnsupportedMathOperationException("Please set a numeric value!");
+                }
+                return (Double) Math.sqrt(converteToDouble(number));
     }
 
     private Double converteToDouble(String strNumber) {
